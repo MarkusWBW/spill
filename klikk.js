c31fa3f2bcb9f418d73f9b54cc21aafc84727563
MariusAaNorheim <131137175+MariusAaNorheim@users.noreply.github.com>
2024-03-07 09:32:40 +0100

    Add files via upload

--- klikk.js
+++ klikk.js
@@ -1,37 +1,58 @@
 // Velg HTML-elementer
-const clickButton = document.getElementById("klikk-knapp");
+const circle1 = document.getElementById("sirkel1");
+const circle2 = document.getElementById("sirkel2");
 const time = document.getElementById("tid");
 const count = document.getElementById("tall");
 const resetButton = document.getElementById("restart-knapp");
-const counter = document.getElementById("teller");
+const startButton = document.getElementById("start-knapp")
 
 // Sett variabler
 let remainingTime = 10;
-let clickCount = 0;
+let clickCount1 = 0;
+let clickCount2 = 0;
 let timer;
 let gameEnded = false; 
 
-// Les antall klikk fra localStorage hvis det finnes
-if(!localStorage.getItem("clickCount")){
-  clickCount = localStorage.getItem("clickCount");
-  count.textContent = clickCount;
-  counter.textContent = "Du har klikket " + clickCount + " ganger!";
-}
 
-// Lytt etter klikk på knappen
-clickButton.addEventListener("click", () => {
+
+// Lytt etter klikk på sirkel 1
+circle1.addEventListener("click", () => {
   if (!gameEnded) {
-    clickCount++;
-    count.textContent = clickCount;
-    counter.textContent = "Du har klikket " + clickCount + " ganger";
-    localStorage.setItem("clickCount", clickCount);
+    clickCount1++;
+    document.getElementById("score1").textContent = clickCount1;
+    count.textContent = totalClickCount;
+    
+  }
+});
+
+// Lytt etter klikk på sirkel 2
+circle2.addEventListener("click", () => {
+  if (!gameEnded) {
+    clickCount2++;
+    document.getElementById("score2").textContent = clickCount2;
+    count.textContent = totalClickCount;
+  }
+});
+
+// Lytt etter tastetrykk
+document.addEventListener("keydown", event => {
+  const key = event.key.toLowerCase();
+  if (key === "s") {
+    circle1.click();
+  } else if (key === "l") {
+    circle2.click();
   }
 });
 
 // Lytt etter klikk på reset-knappen
 resetButton.addEventListener("click", () => {
   resetGame();
+  //window.location.reload()
 });
+
+/* resetButton.addEventListener("click", () => {
+  startTimer();
+}); */
 
 // Start nedtelling av tid
 function startTimer() {
@@ -42,30 +63,43 @@ function startTimer() {
       clearInterval(timer);
       endGame();
     }
+
   }, 1000);
 }
+
+
+
+
 
 // Avslutt spillet
 function endGame() {
   gameEnded = true;
-  clickButton.style.disabled = "none";
+  circle1.style.disabled = "none";
+  circle2.style.disabled = "none";
   resetButton.style.display = "block";
-  counter.textContent = "Du klarte å klikke " + clickCount + " ganger!";
 }
 
 // Tilbakestill spillet
 function resetGame() {
   gameEnded = false;
+  startTimer()
   remainingTime = 10;
-  clickCount = 0;
-  time.textContent = remainingTime;
-  count.textContent = clickCount;
-  clickButton.style.display = "block";
-  resetButton.style.display = "none";
-  counter.textContent = "Du har klikket " + clickCount + " ganger";
-  localStorage.removeItem("clickCount");
-  startTimer();
+  clickCount1 = 0;
+  clickCount2 = 0;
+  
+  document.getElementById("score1").textContent = clickCount1;
+  document.getElementById("score2").textContent = clickCount2;
+  count.textContent = totalClickCount;
+  circle1.style.display = "block";
+  circle2.style.display = "block";
+  resetButton.style.display = "restart-knapp";
+
+  
+  
 }
 
 // Start spillet
 startTimer();
+
+
+
